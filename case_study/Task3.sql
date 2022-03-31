c57a5770282a38e8138c6eeb32378f091bc50713
Nguyen <nguyen2697s@gmail.com>
2022-03-31 10:54:15 +0700

    task 45 cs1

--- case_study/Task3.sql
+++ case_study/Task3.sql
@@ -1,5 +1,5 @@
 USE furama_resort;
-SELECT customer_id,customer_name,TIMESTAMPDIFF(YEAR, date_of_birth, CURDATE()),address AS age
+SELECT customer_id,customer_name,TIMESTAMPDIFF(YEAR, date_of_birth, CURDATE())AS age,address
 FROM customer
 WHERE (TIMESTAMPDIFF(YEAR, date_of_birth, CURDATE()) BETWEEN 18 AND 50)
   AND (address LIKE '%Đà Nẵng' OR address LIKE '%Quảng Trị');
---
+++ case_study/Task5.sql
@@ -0,0 +1,22 @@
+USE furama_resort;
+
+SELECT c2.customer_id                                       AS cid,
+       c2.customer_name,
+       ct.customer_type_name                                AS 'type',
+       c.contract_id,
+       s.service_name,
+       c.created_date,
+       c.end_date,
+       SUM(COALESCE(`as`.price * cd.quantity, 0) + s.price) AS 'total'
+
+FROM contract c
+         RIGHT JOIN customer c2 ON c.customer_id = c2.customer_id
+         LEFT JOIN contract_detail cd ON c.contract_id = cd.contract_id
+         LEFT JOIN customer_type ct ON c2.customer_type_id = ct.customer_type_id
+         LEFT JOIN service s ON c.service_id = s.service_id
+         LEFT JOIN service_type st ON s.service_type_id = st.service_type_id
+         LEFT JOIN accompanied_service `as` ON cd.accompanied_service_id = `as`.accompanied_service_id
+GROUP BY c.contract_id, c2.customer_id
+ORDER BY cid, c.contract_id DESC;
+;
+
