90de2e3e2e6cd553a6e781c940c1e6abff5aab73
Nguyen <nguyen2697s@gmail.com>
2022-04-12 10:04:27 +0700

    add thuc hanh ss9

---
+++ ss9_tong_quan_ung_dung_web/thuc_hanh/ung_dung_tu_dien_don_gian/src/main/java/TranslationServlet.java
@@ -0,0 +1,59 @@
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "TranslationServlet", value = "/translate")
+public class TranslationServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Map<String, String> dictionary = new HashMap<>();
+        dictionary.put("hello", "Xin chào");
+        dictionary.put("how", "Thế nào");
+        dictionary.put("book", "Quyển vở");
+        dictionary.put("computer", "Máy tính");
+
+        String search = request.getParameter("txtSearch");
+
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"/>");
+        String result = dictionary.get(search);
+        if(result != null){
+            writer.println("Word: " + search);
+            writer.println("Result: " + result);
+        } else {
+            writer.println("Not found");
+        }
+
+        writer.println("</html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Map<String, String> dictionary = new HashMap<>();
+        dictionary.put("hello", "Xin chào");
+        dictionary.put("how", "Thế nào");
+        dictionary.put("book", "Quyển vở");
+        dictionary.put("computer", "Máy tính");
+
+        String search = request.getParameter("txtSearch");
+
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"/>");
+        String result = dictionary.get(search);
+        if(result != null){
+            writer.println("Word: " + search);
+            writer.println("Result: " + result);
+        } else {
+            writer.println("Not found");
+        }
+
+        writer.println("</html>");
+    }
+}
